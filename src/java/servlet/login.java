4486bbaa6c97ab812ac921db46d5cb1cbac69149
sfuen <sfuen@DESKTOP-15INNQK.lan>
2021-05-23 00:04:14 -0400

    Login Creado con Redireccionamiento Correspondiente, Solo Falataria Cerrar Sesion y Que no se pueda entrar a otras paginas por url

--- src/java/servlet/login.java
+++ src/java/servlet/login.java
@@ -1,114 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import servicio.User;
+import ConexionconBD.ConexionBD;
 
-/**
- *
- * @author norar
- */
 public class login extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet login</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet login at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
         HttpSession s = request.getSession();
         
         User u = (User)s.getAttribute("user");
         if(u==null)
             request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
         else{
-           // String nombre = u.getActivo().getNombre();            
-           // request.setAttribute("nombre", nombre);
-            request.getRequestDispatcher("WEB-INF/agregarUsuario.jsp").forward(request, response);
+           request.getRequestDispatcher("WEB-INF/agregarUsuario.jsp").forward(request, response);
         }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
-        HttpSession s = request.getSession();
-        String usuario = request.getParameter("uname");
-        String pass = request.getParameter("psw");
+        HttpSession sesion = request.getSession();
         
-        User u = new User();
+        ConexionBD conec = new ConexionBD();
+        String email = request.getParameter("txtEmail");
+        String pass = request.getParameter("txtPass");
+        int tipoUser = conec.loguear(email, pass);
         
-        if(u.login(usuario, pass)){
-            s.setAttribute("user", u);
-          //  String nombre = u.getActivo().getNombre();            
-          //  request.setAttribute("nombre", nombre);
-            request.getRequestDispatcher("WEB-INF/agregarUsuario.jsp").forward(request, response);
-        }else{
-            request.setAttribute("mensaje", "Datos incorrectos");
-            request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
+        if(tipoUser == 2){
+            sesion.setAttribute("email", email);
+            sesion.setAttribute("Tipousuario", tipoUser);
+            int idProfesional = conec.obtenerIdProfesional(email, pass);
+            sesion.setAttribute("idProfesional", idProfesional);
+            response.sendRedirect("profesional");
+        }else if(tipoUser == 3){
+            sesion.setAttribute("email", email);
+            sesion.setAttribute("Tipousuario", tipoUser);
+            response.sendRedirect("cliente");
+        }
+        if(request.getParameter("cerrar") != null){
+            sesion.invalidate();
         }
         
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
