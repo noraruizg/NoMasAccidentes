dc8cd38d051521b340bb473df05d4742777efae4
sfuen <>
2021-05-26 12:26:54 -0400

    Metodo Para Agregar CheckListAsesoria

--- src/java/servlet/checklistAsesoria.java
+++ src/java/servlet/checklistAsesoria.java
@@ -1,87 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlet;
 
+import ConexionconBD.ConexionBD;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author norar
- */
 public class checklistAsesoria extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet checklistAsesoria</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet checklistAsesoria at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
         request.getRequestDispatcher("WEB-INF/asesoria/checklistAsesoria.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        
+        HttpSession sesion = request.getSession();
+        
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-MM-yyyy");
+        
+        String fechaChAs = request.getParameter("fechaChecklistAsesoria");
+        
+        java.util.Date fechaChkAsesoria = null;
+        try {
+            fechaChkAsesoria = formatoFecha.parse(fechaChAs);
+        } catch (ParseException ex) {
+            Logger.getLogger(agregarContrato.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        java.sql.Date fechaChekListAsesoria = new java.sql.Date(fechaChkAsesoria.getTime());
+        
+        String CausanteChecklist = request.getParameter("causanteAsesoria");
+        String mejoracheckList = request.getParameter("mejoraCheckListAsesoria");
+        int IdAsesoria = Integer.parseInt(request.getParameter("idAsesoria"));
+        
+        ConexionBD conec = new ConexionBD();
+        Connection conn = conec.conectar();
+        
+        try{  
+            CallableStatement cst = conn.prepareCall("{call RegistrarCheckListAsesoria(?,?,?,?,?)}");
+            cst.setDate(1, fechaChekListAsesoria);
+            cst.setString(2, CausanteChecklist);
+            cst.setString(3, mejoracheckList);
+            cst.setString(4, "T"); 
+            cst.setInt(5, IdAsesoria); 
+            cst.executeUpdate(); 
+        }catch(SQLException ex){
+            System.out.println("Error de SQL" + ex);
+        }
+        response.sendRedirect("listarAsesoria");
+        
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
